3f93d7ecaced0e1c544c1376aa595660896cba96
Tasluf665 <tasluf665@gmail.com>
2026-01-11 16:35:32 +0100

    Mobile App Init

---
+++ mobile/app/(Authentication)/SplashScreen.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { API_URL } from '@env'
+
+export default function SplashScreen() {
+
+    return (
+        <View style={styles.container}  >
+            <Text>SplashScreen</Text>
+            <Text>{API_URL}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
---
+++ mobile/app/(Authentication)/_layout.js
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+export default () => {
+  return (
+    <Stack
+      initialRouteName="SplashScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="SplashScreen" />
+    </Stack>
+  );
+};
